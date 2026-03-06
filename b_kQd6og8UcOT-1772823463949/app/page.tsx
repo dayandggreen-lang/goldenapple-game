14dc4f9d55a4844382bbc9e4ca29d513db5f95da
dayandggreen-lang <dayandggreen@gmail.com>
2026-03-06 22:08:01 +0300

    Delete b_kQd6og8UcOT-1772823463949 directory

--- b_kQd6og8UcOT-1772823463949/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <iframe 
-      src="/index.html" 
-      className="w-full h-screen border-0"
-      title="Dad's Apple Catch Game"
-    />
-  );
-}
